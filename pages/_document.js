ade574c06cde0a63939580957d2dc5ce620191ef
itsupport-sapocom <135614023+itsupport-sapocom@users.noreply.github.com>
2023-07-25 09:33:37 +0530

    _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import Script from 'next/script'
+ 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+	   <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-YHB4RGD380');
+        `}
+      </Script>
       <body>
         <Main />
         <NextScript />
